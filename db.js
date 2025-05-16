4eec2ae6a89dda7071068c1f067878738201b189
Davo11030 <rdfh1103@gmail.com>
2025-05-16 02:12:02 -0700

    space

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     // {{{{       MODIFICAR CREDENCIALES SEGUN SEA EL CASO      }}}}
-  host: 'localhost',
-  user: 'root',
-  password: 'luchas1.',
-  database: 'sistemaasesoria'
+    host: 'localhost',
+    user: 'root',
+    password: 'luchas1.',
+    database: 'sistemaasesoria'
 });
 
 module.exports = db;
