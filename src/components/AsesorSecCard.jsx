b91ba109278cea271f3522d1f08d57029718e869
uluxeGit <al22760594@ite.edu.mx>
2025-05-16 13:24:20 -0700

    Actualizacion: cambios menores de apariencia en tarjetas

--- src/components/AsesorSecCard.jsx
+++ src/components/AsesorSecCard.jsx
@@ -4,10 +4,10 @@ const AsesorSecCard = ({tema, nombre, fechaAcordada, lugar, onModificar, onFinal
     return (
         <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl border rounded-lg p-4 shadow-md bg-[#BDD4E7] ">
             
-            <h3 className="text-lg font-bold"><strong>Tema:</strong> {tema}</h3>
-            <p><strong>Nombre alumno: </strong>{nombre}</p>
-            <p><strong>Fecha acordada: </strong> {fechaAcordada}</p>
-            <p><strong>Lugar: </strong> {lugar}</p>
+            <h3 className="text-xs font-bold">Tema: {tema}</h3>
+            <p className='text-xs'>Nombre alumno: {nombre}</p>
+            <p className='text-xs'>Fecha acordada: {fechaAcordada}</p>
+            <p className='text-xs'>Lugar: {lugar}</p>
             
             <div className="flex flex-col md:flex-row justify-between gap-2 mt-4">
                 <button onClick={onModificar} className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600">
