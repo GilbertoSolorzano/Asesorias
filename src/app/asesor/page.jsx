4a7ab43ced2059140b52cf26fac45e865a5f21e8
victorHerediaa <al22760587@ite.edu.mx>
2025-05-30 14:41:59 -0700

    diseno del chat asesor

--- src/app/asesor/page.jsx
+++ src/app/asesor/page.jsx
@@ -212,7 +212,7 @@ const AsesorPage = () => {
 
       {/* Chat */}
       {chatRoom && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="fixed inset-0  flex justify-center items-center z-50">
           <div className="bg-white rounded-lg shadow-lg max-w-xl w-full h-[80vh] p-4 relative">
             <button
               onClick={() => setChatRoom(null)}
